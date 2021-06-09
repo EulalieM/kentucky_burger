13b6a695704cffadd979d27c5fbb0b9deffc709a
Eulalie Moreau <eulalie.m16000@gmail.com>
2021-06-09 13:01:25 +0200

    Update products_nav page

--- resources/views/products/products_nav.blade.php
+++ resources/views/products/products_nav.blade.php
@@ -2,12 +2,10 @@
 
 @section('content')
 
-<div>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
+<div class="products_nav-content">
+    <a href="{{ route('products') }}"><img src="/img/burgers.png" alt="Nos burgers"></a>
+    <a href="{{ route('products') }}"><img src="/img/boissons.png" alt="Nos boissons"></a>
+    <a href="{{ route('products') }}"><img src="/img/desserts.png" alt="Nos desserts"></a>
 </div>
 
 @endsection
